58ca6898b85d54edcd942ff21625d404418d7328
052787 <052787@SPFAM2LAB161-20.famacad.com.br>
2022-09-20 20:00:31 -0300

    Exclusão = aula 20/9/2022

---
+++ src/main/java/br/vemprafam/servlet/ServletExclusao.java
@@ -0,0 +1,56 @@
+package br.vemprafam.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.vemprafam.dao.DaoFuncionario;
+import br.vemprafam.pojo.Funcionario;
+
+/**
+ * Servlet implementation class ServletExclusao
+ */
+@WebServlet("/excluirFuncionario")
+public class ServletExclusao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletExclusao() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int re = Integer.parseInt(request.getParameter("re"));
+		DaoFuncionario dao = new DaoFuncionario();
+		dao.excluirFuncionario(new Funcionario(re,"",null,0.0));
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Excluído!<br/>");
+		out.println("<a href='/Projeto'>voltar</a>");
+		out.println("</body>");
+		out.println("</html>");	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
